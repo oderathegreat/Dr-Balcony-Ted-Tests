278508ebab72aa97371c348c46fb387d1a114637
oderathegreat <oderathegreat@gmail.com>
2024-06-14 14:55:16 +0300

    fecth input and filter based on what user has typed

--- getUsers.php
+++ getUsers.php
@@ -3,3 +3,12 @@
 
 //a list of users in array to just simulate the task
 $users = ['Mike Dave', 'Jimmy iovine', 'Malia Janny', 'Ali rakim', 'Bob Brown', 'Christopher Sanders', 'Wakanda Manna', 'Dave Paul', 'Eilein mima', 'Fay rahs'];
+
+
+//fetch the input
+$q = $_GET['q'];
+
+// filter based on what user inputs
+$filteredUsers = array_filter($users, function($user) use ($q) {
+    return stripos($user, $q) !== false;
+  });
